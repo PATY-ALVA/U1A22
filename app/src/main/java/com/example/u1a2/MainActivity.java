105f271f46de2c1b39f3137aa2c9fa0a3dbe4dc3
patyAlva <pao_tu.angelguardian@yahoo.com>
2019-04-19 12:17:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/u1a2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.u1a2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+package unam.fca.dmoviles;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.Button;
+        import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
